88eefec93195a837a0b19466e4a321b17bafcc21
Bonita Scott <bonitalscott@yahoo.com>
2021-05-04 15:48:09 -0400

    Initial Push to Heroku

--- db.js
+++ db.js
@@ -1,8 +1,20 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('red-badge-project', 'postgres', 'password', {
-  host: 'localhost',
+const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres'
 });
 
 module.exports = db;
+
+// original code before Heroku Deployment
+// const db = new Sequelize('red-badge-project', 'postgres', 'password', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+// note: passwords with special characters OTHER THAN !, must be encoded, and
+// MUST be Added to .env file like this PASS = IktpIh4u#
+// Then in this file lines 3-5 would look like this (Note the back ticks!)
+// const db = new Sequelize(process.env.DATABASE_URL ||
+// `postgresql://postgres://${encodeURIComponent(process.env.PASS)}`, {
+//   dialect: 'postgres'
+// });
